781b9bac05ee567ffefb077f1a8bb6dfca808976
Earnest Abrom <earnestabromjr@gmail.com>
2023-09-06 20:38:47 +0000

    changes to intro go code

--- packages/basics/main.go
+++ packages/basics/main.go
@@ -1,5 +1,16 @@
 // packages/basics/main.go
 package main
 
+// import the fmt package from the standard library
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+// use the fmt package to print the string "Hello Gophers!"
+fmt.Println("Hello Gopher")
+
+// use the time package to print the current weekday
+fmt.Printf("Today is %s", time.Now().Weekday())
 }
